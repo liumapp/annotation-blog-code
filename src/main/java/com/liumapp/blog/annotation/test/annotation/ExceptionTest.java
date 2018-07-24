763465686a3e4beff25b05d4da185049344b7efe
liumapp <liumapp.com@gmail.com>
2018-07-24 09:37:37 +0800

    add exception test

---
+++ src/main/java/com/liumapp/blog/annotation/test/annotation/ExceptionTest.java
@@ -0,0 +1,21 @@
+package com.liumapp.blog.annotation.test.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author liumapp
+ * @file ExceptionTest.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/24/18
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ExceptionTest {
+
+    Class<? extends Exception> value();
+
+}
