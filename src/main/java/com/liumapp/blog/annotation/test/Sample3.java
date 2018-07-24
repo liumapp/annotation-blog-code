8f3c55190c59a4be36149d502dd764eb54968352
liumapp <liumapp.com@gmail.com>
2018-07-24 19:57:50 +0800

    ready to test multy exception annotation

---
+++ src/main/java/com/liumapp/blog/annotation/test/Sample3.java
@@ -0,0 +1,11 @@
+package com.liumapp.blog.annotation.test;
+
+/**
+ * @author liumapp
+ * @file Sample3.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/24/18
+ */
+public class Sample3 {
+}
