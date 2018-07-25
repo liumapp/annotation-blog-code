0a5926142ff13dd083c7b512d6344e03f3bfc469
liumapp <liumapp.com@gmail.com>
2018-07-25 23:20:32 +0800

    add run sample4 test

---
+++ src/main/java/com/liumapp/blog/annotation/exception/RunSample4Tests.java
@@ -0,0 +1,21 @@
+package com.liumapp.blog.annotation.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author liumapp
+ * @file RunSample4Tests.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/25/18
+ */
+public class RunSample4Tests {
+
+    public static Logger logger = LoggerFactory.getLogger(RunSample4Tests.class);
+
+    public static void main (String[] args) {
+        
+    }
+
+}
