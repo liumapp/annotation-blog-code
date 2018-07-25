be9697d6b91b69127b68d432efa1d796cafffb14
liumapp <liumapp.com@gmail.com>
2018-07-25 20:22:19 +0800

    add how to get annotation

--- src/main/java/com/liumapp/blog/annotation/basic/Man.java
+++ src/main/java/com/liumapp/blog/annotation/basic/Man.java
@@ -1,6 +1,8 @@
 package com.liumapp.blog.annotation.basic;
 
 import com.liumapp.blog.annotation.basic.annotation.HumanAnnotation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author liumapp
@@ -12,6 +14,16 @@ import com.liumapp.blog.annotation.basic.annotation.HumanAnnotation;
 @HumanAnnotation(name = "Zhangsan", sex = "boy", age = 18)
 public class Man {
 
+    private static Logger logger = LoggerFactory.getLogger(Man.class);
 
+    public static void main (String[] args) {
+        boolean hasAnnotation = Man.class.isAnnotationPresent(HumanAnnotation.class);
+        if (hasAnnotation) {
+            HumanAnnotation humanAnnotation = Man.class.getAnnotation(HumanAnnotation.class);
+            logger.info("a human named :  " + humanAnnotation.name());
+            logger.info("a human who is a : " + humanAnnotation.sex());
+            logger.info("a human who is : " + humanAnnotation.age() + " years old");
+        }
+    }
 
 }
